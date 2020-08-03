22663d2c4c3b65562cd743541724285dd08de5a4
Yz <yanzhen610@gmail.com>
2020-08-03 17:18:30 +0800

    updated random string

--- utils/randstr.js
+++ utils/randstr.js
@@ -1,12 +1,24 @@
-const availableChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-const availableCharsLength = availableChars.length;
+const defaultCharSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
 
-function randstr(length) {
+/**
+ * Generate random string.
+ *
+ * @param {integer} length The length of the random string to be generated.
+ * @param {array|string} charSet The characters used to generate the random string.
+ * @returns {string}
+ */
+function randStr(length, charSet = defaultCharSet) {
   const result = new Array(length);
-  for (let i = 0; i < length; ++i) {
-    result[i] = availableChars.charAt(Math.trunc(Math.random() * availableCharsLength));
+  if (typeof charSet === 'string') {
+    for (let i = 0; i < length; ++i) {
+      result[i] = charSet.charAt(Math.trunc(Math.random() * charSet.length));
+    }
+  } else {
+    for (let i = 0; i < length; ++i) {
+      result[i] = charSet[Math.trunc(Math.random() * charSet.length)];
+    }
   }
   return result.join('');
 }
 
-module.exports = randstr;
+module.exports = randStr;
